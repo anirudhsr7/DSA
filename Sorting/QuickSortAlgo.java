6b5ba0771b30f61f139fb3d7c72b7709a6305b2d
Anirudh Singh <41021363+anirudhsr7@users.noreply.github.com>
2021-01-30 19:49:33 +0530

    Create QuickSortAlgo.java
    Quick Sort Algo
    Time Complexity: O(nlogn) in Average Case
    Space Complexity: O(1)
    In-Place Sorting Algo

---
+++ Sorting/QuickSortAlgo.java
@@ -0,0 +1,50 @@
+public class QuickSortAlgo {
+	
+	public static void swap(int[] array, int a, int b) {
+		int temp = array[a];
+		array[a] = array[b];
+		array[b] = temp;
+	}
+	
+	public static int partition(int[] array, int start, int end) {
+		int pivot = array[end];
+		int pIndex = start;
+		
+		for(int i=start; i<=end-1; i++) {
+			if(array[i]<=pivot) {
+				swap(array, pIndex, i);
+				pIndex += 1;
+			}
+		}
+		swap(array, pIndex, end);
+		return pIndex;
+		
+	}
+	
+	public static void quickSort(int[] array, int start, int end) {
+		if(start<end) {
+			
+			int pIndex = partition(array, start, end);
+//			System.out.println(pIndex);
+			quickSort(array, start, pIndex-1);
+			quickSort(array, pIndex+1, end);
+		}
+	}
+	
+	public static void printArray(int[] array, int size) {
+		for(int i=0; i<size; i++) {
+			System.out.print(array[i]+" ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {1,4,2,7,8,5,6};
+		int size = array.length;
+		
+		printArray(array, size);
+		System.out.println("\nAfter Sorting: ");
+		quickSort(array, 0, size-1);
+		printArray(array, size);
+		
+	}
+}
